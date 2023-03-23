239d9795188357e575db5083eb258f26bf5933fe
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2023-03-23 17:15:14 +0100

    Merge branch 'CalendarioAdminv3'

--- Proyecto_Principal/aulanosa_app/lib/admin/admin_proyectos_tutorias.dart
+++ Proyecto_Principal/aulanosa_app/lib/admin/admin_proyectos_tutorias.dart
@@ -1,4 +1,5 @@
-// ignore_for_file: use_key_in_widget_constructors, camel_case_types, prefer_typing_uninitialized_variables, prefer_const_constructors, annotate_overrides, unused_import, unnecessary_import, non_constant_identifier_names, prefer_interpolation_to_compose_strings, prefer_const_literals_to_create_immutables, body_might_complete_normally_nullable, avoid_unnecessary_containers, sort_child_properties_last, unused_element, curly_braces_in_flow_control_structures, avoid_function_literals_in_foreach_calls
+// ignore_for_file: use_key_in_widget_constructors, camel_case_types, prefer_typing_uninitialized_variables, prefer_const_constructors, annotate_overrides, unused_import, unnecessary_import, non_constant_identifier_names, prefer_interpolation_to_compose_strings, prefer_const_literals_to_create_immutables, body_might_complete_normally_nullable, avoid_unnecessary_containers, sort_child_properties_last, unused_element, curly_braces_in_flow_control_structures
+import 'package:aulanosa_app/globals/variable_global.dart';
 import 'package:aulanosa_app/objetosNecesarios/mensaje.dart';
 import 'package:aulanosa_app/objetosNecesarios/menu_widget.dart';
 import 'package:flutter/material.dart';
@@ -23,34 +24,132 @@ class TutoriasAdminState extends State<TutoriasAdmin> {
   late double anchoPantalla;
   DateTime hoy = DateTime.now();
   DateTime diaSeleccionado = DateTime.now();
+  
 
-  List<String> listaEventos = [];
+  
    
-  //Metodo que se llamara cuando seleccionemos cualquier dia
-  dia_seleccionadoAdmin(DateTime dia, DateTime diaFocus) {
+  
+
+  double topCalendario = 2;
+  bool crearTutoria = false;
+  List<Mensaje> listadoTotalMensajes = [];
+  bool retornarCalendario = true;
+  bool retornarTutoria = false;
+  final _formKey = GlobalKey<FormState>();
+  
+
+  
+   Mensaje mensaje1 = Mensaje(id: 1, idUsuario: 1, asunto: "Este es el asunto del mensaje1", texto: "Texto del mensaje1", fecha: DateTime.utc(4, 3, 2023));
+   Mensaje mensaje2 = Mensaje(id: 2, idUsuario: 2, asunto: "Este es el asunto del mensaje2", texto: "Texto del mensaje2", fecha: DateTime.utc(10, 3, 2023));
+   Mensaje mensaje3 = Mensaje(id: 3, idUsuario: 3, asunto: "Este es el asunto del mensaje3", texto: "Texto del mensaje3", fecha: DateTime.utc(4, 3, 2023));
+   Mensaje mensaje4 = Mensaje(id: 4, idUsuario: 4, asunto: "Este es el asunto del mensaje4", texto: "Texto del mensaje4", fecha: DateTime.utc(21, 3, 2023));
+   Mensaje mensaje5 = Mensaje(id: 5, idUsuario: 5, asunto: "Este es el asunto del mensaje5", texto: "Texto del mensaje5", fecha: DateTime.utc(20, 4, 2023));
+   Mensaje mensaje6 = Mensaje(id: 6, idUsuario: 6, asunto: "Este es el asunto del mensaje6", texto: "Texto del mensaje6", fecha: DateTime.utc(10, 3, 2023));
+   Mensaje mensaje7 = Mensaje(id: 7, idUsuario: 7, asunto: "Este es el asunto del mensaje7", texto: "Texto del mensaje7", fecha: DateTime.utc(4, 3, 2023));
+   Mensaje mensaje8 = Mensaje(id: 8, idUsuario: 8, asunto: "Este es el asunto del mensaje8", texto: "Texto del mensaje8", fecha: DateTime.utc(7, 5, 2023));
+   Mensaje mensaje9 = Mensaje(id: 9, idUsuario: 9, asunto: "Este es el asunto del mensaje9", texto: "Texto del mensaje9", fecha: DateTime.utc(9, 3, 2023));
+   Mensaje mensaje10 = Mensaje(id: 10, idUsuario: 10, asunto: "Este es el asunto del mensaje10", texto: "Texto del mensaje10", fecha: DateTime.utc(22, 3, 2023));
+   Mensaje mensaje11 = Mensaje(id: 11, idUsuario: 11, asunto: "Este es el asunto del mensaje11", texto: "Texto del mensaje11", fecha: DateTime.utc(30, 4, 2023));
+   Mensaje mensaje12 = Mensaje(id: 12, idUsuario: 12, asunto: "Este es el asunto del mensaje12", texto: "Texto del mensaje12", fecha: DateTime.utc(22, 3, 2023));
+   Mensaje mensaje13 = Mensaje(id: 13, idUsuario: 13, asunto: "Este es el asunto del mensaje13", texto: "Texto del mensaje13", fecha: DateTime.utc(20, 1, 2023));
+   Mensaje mensaje14 = Mensaje(id: 14, idUsuario: 14, asunto: "Este es el asunto del mensaje14", texto: "Texto del mensaje14", fecha: DateTime.utc(17, 1, 2023));
+   Mensaje mensaje15 = Mensaje(id: 15, idUsuario: 15, asunto: "Este es el asunto del mensaje15", texto: "Texto del mensaje15", fecha: DateTime.utc(15, 3, 2023));
+   Mensaje mensaje16 = Mensaje(id: 16, idUsuario: 16, asunto: "Este es el asunto del mensaje16", texto: "Texto del mensaje16", fecha: DateTime.utc(12, 1, 2023));
+   Mensaje mensaje17 = Mensaje(id: 17, idUsuario: 17, asunto: "Este es el asunto del mensaje17", texto: "Texto del mensaje17", fecha: DateTime.utc(12, 3, 2023));
+   Mensaje mensaje18 = Mensaje(id: 18, idUsuario: 18, asunto: "Este es el asunto del mensaje18", texto: "Texto del mensaje18", fecha: DateTime.utc(17, 3, 2023));
+   Mensaje mensaje19 = Mensaje(id: 19, idUsuario: 19, asunto: "Este es el asunto del mensaje19", texto: "Texto del mensaje19", fecha: DateTime.utc(27, 3, 2023));
+  
+  List<Mensaje> listaEventos = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    listaEventos.add(mensaje1);
+    listaEventos.add(mensaje2);
+    listaEventos.add(mensaje3);
+    listaEventos.add(mensaje4);
+    listaEventos.add(mensaje5);
+    listaEventos.add(mensaje6);
+    listaEventos.add(mensaje7);
+    listaEventos.add(mensaje8);
+    listaEventos.add(mensaje9);
+    listaEventos.add(mensaje10);
+    listaEventos.add(mensaje11);
+    listaEventos.add(mensaje12);
+    listaEventos.add(mensaje13);
+    listaEventos.add(mensaje14);
+    listaEventos.add(mensaje15);
+    listaEventos.add(mensaje16);
+    listaEventos.add(mensaje17);
+    listaEventos.add(mensaje18);
+    listaEventos.add(mensaje19);
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    //Reogememos las medidas de alto y ancho del dispositivo para poder trabajar siempre con medidas RELATIVAS
     setState(() {
-      hoy = dia;
+      size = MediaQuery.of(context).size;
+      alturaPantalla = size.height;
+      anchoPantalla = size.width;
     });
-    diaSeleccionado = dia;
-    showDialog(context: context, builder: (BuildContext context) {
+    
+    DropdownButtonFormField(items: <DropdownMenuItem>[], onChanged: (value) {});
+
+
+    void retornarLista(List<Mensaje>listadoMensajes, DateTime dia) {
+      List<Mensaje> listadoMensajesDia = [];
+      for(int i = 0; i < listadoMensajes.length; i ++){
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 1, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 2, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 3, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+        }
+
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 4, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 5, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 6, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 7, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 8, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 9, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 10, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 11, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+        if(listadoMensajes[i].fecha == DateTime.utc((dia.day).toInt(), 12, (DateTime.now().year).toInt())){
+          listadoMensajesDia.add(listadoMensajes[i]);
+           }
+   
+   }
+     showDialog(context: context, builder: (BuildContext context) {
       return StatefulBuilder(builder:(context, StateSetter setState) {
         return Container(padding: EdgeInsets.all(10),
         child: ListView.builder(
-          itemCount: 4,
+          itemCount: listadoMensajesDia.length,
           itemBuilder: (context, index) {
           
-          if(index == 0) {
-            return Container(
-            alignment:Alignment.center,
-              height: 200,
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text("Lista de eventos", style:TextStyle(color: Colors.white, fontWeight: FontWeight.bold, decoration: TextDecoration.none, ), textAlign: TextAlign.center,)
-            );
-        }
-        else {
+        
+        
           return Card(
             margin: EdgeInsets.only(top: 20),
             child: Container(
@@ -66,111 +165,30 @@ class TutoriasAdminState extends State<TutoriasAdmin> {
                 decoration: BoxDecoration( border: Border(bottom: BorderSide(color: Colors.blueGrey, width: 3), top: BorderSide.none, left: BorderSide.none, right: BorderSide.none)),
                 alignment: Alignment.topCenter,
                 margin: EdgeInsets.only(top: 80),
-                child: Text("Informacion evento", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,  color: Colors.blueGrey))
+                child: Text(listadoMensajesDia[index].asunto, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,  color: Colors.blueGrey))
               )
           ],)
 )
       );
+      
+    
     }
-        },));
+        ,));
       },);
     });
-  }
-
-  double topCalendario = 2;
-  bool crearTutoria = false;
- 
-  bool retornarCalendario = true;
-  bool retornarTutoria = false;
-  final _formKey = GlobalKey<FormState>();
-
-  
-   Mensaje mensaje1 = Mensaje(id: 1, idUsuario: 1, asunto: "Este es el asunto del mensaje1", texto: "Texto del mensaje1", fecha: DateTime.utc(4, 3, 2023));
-   Mensaje mensaje2 = Mensaje(id: 2, idUsuario: 2, asunto: "Este es el asunto del mensaje2", texto: "Texto del mensaje2", fecha: DateTime.utc(14, 3, 2023));
-   Mensaje mensaje3 = Mensaje(id: 3, idUsuario: 3, asunto: "Este es el asunto del mensaje3", texto: "Texto del mensaje3", fecha: DateTime.utc(13, 3, 2023));
-   Mensaje mensaje4 = Mensaje(id: 4, idUsuario: 4, asunto: "Este es el asunto del mensaje4", texto: "Texto del mensaje4", fecha: DateTime.utc(5, 3, 2023));
-   Mensaje mensaje5 = Mensaje(id: 5, idUsuario: 5, asunto: "Este es el asunto del mensaje5", texto: "Texto del mensaje5", fecha: DateTime.utc(2, 3, 2023));
-   Mensaje mensaje6 = Mensaje(id: 6, idUsuario: 6, asunto: "Este es el asunto del mensaje6", texto: "Texto del mensaje6", fecha: DateTime.utc(5, 3, 2023));
-   Mensaje mensaje7 = Mensaje(id: 7, idUsuario: 7, asunto: "Este es el asunto del mensaje7", texto: "Texto del mensaje7", fecha: DateTime.utc(2, 3, 2023));
-   Mensaje mensaje8 = Mensaje(id: 8, idUsuario: 8, asunto: "Este es el asunto del mensaje8", texto: "Texto del mensaje8", fecha: DateTime.utc(4, 3, 2023));
-   Mensaje mensaje9 = Mensaje(id: 9, idUsuario: 9, asunto: "Este es el asunto del mensaje9", texto: "Texto del mensaje9", fecha: DateTime.utc(6, 3, 2023));
-   Mensaje mensaje10 = Mensaje(id: 10, idUsuario: 10, asunto: "Este es el asunto del mensaje10", texto: "Texto del mensaje10", fecha: DateTime.utc(10, 3, 2023));
-   Mensaje mensaje11 = Mensaje(id: 11, idUsuario: 11, asunto: "Este es el asunto del mensaje11", texto: "Texto del mensaje11", fecha: DateTime.utc(11, 3, 2023));
-   Mensaje mensaje12 = Mensaje(id: 12, idUsuario: 12, asunto: "Este es el asunto del mensaje12", texto: "Texto del mensaje12", fecha: DateTime.utc(23, 3, 2023));
-   Mensaje mensaje13 = Mensaje(id: 13, idUsuario: 13, asunto: "Este es el asunto del mensaje13", texto: "Texto del mensaje13", fecha: DateTime.utc(16, 3, 2023));
-   Mensaje mensaje14 = Mensaje(id: 14, idUsuario: 14, asunto: "Este es el asunto del mensaje14", texto: "Texto del mensaje14", fecha: DateTime.utc(20, 3, 2023));
-   Mensaje mensaje15 = Mensaje(id: 15, idUsuario: 15, asunto: "Este es el asunto del mensaje15", texto: "Texto del mensaje15", fecha: DateTime.utc(24, 3, 2023));
-   Mensaje mensaje16 = Mensaje(id: 16, idUsuario: 16, asunto: "Este es el asunto del mensaje16", texto: "Texto del mensaje16", fecha: DateTime.utc(20, 3, 2023));
-   Mensaje mensaje17 = Mensaje(id: 17, idUsuario: 17, asunto: "Este es el asunto del mensaje17", texto: "Texto del mensaje17", fecha: DateTime.utc(11, 3, 2023));
-   Mensaje mensaje18 = Mensaje(id: 18, idUsuario: 18, asunto: "Este es el asunto del mensaje18", texto: "Texto del mensaje18", fecha: DateTime.utc(14, 3, 2023));
-   Mensaje mensaje19 = Mensaje(id: 19, idUsuario: 19, asunto: "Este es el asunto del mensaje19", texto: "Texto del mensaje19", fecha: DateTime.utc(12, 3, 2023));
-  
-   final List<Mensaje> listaMensajes = [];
-   
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    listaMensajes.add(mensaje1);
-    listaMensajes.add(mensaje2);
-    listaMensajes.add(mensaje3);
-    listaMensajes.add(mensaje4);
-    listaMensajes.add(mensaje5);
-    listaMensajes.add(mensaje6);
-    listaMensajes.add(mensaje7);
-    listaMensajes.add(mensaje8);
-    listaMensajes.add(mensaje9);
-    listaMensajes.add(mensaje10);
-    listaMensajes.add(mensaje11);
-    listaMensajes.add(mensaje12);
-    listaMensajes.add(mensaje13);
-    listaMensajes.add(mensaje14);
-    listaMensajes.add(mensaje15);
-    listaMensajes.add(mensaje16);
-    listaMensajes.add(mensaje17);
-    listaMensajes.add(mensaje18);
-    listaMensajes.add(mensaje19);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //Reogememos las medidas de alto y ancho del dispositivo para poder trabajar siempre con medidas RELATIVAS
-    setState(() {
-      size = MediaQuery.of(context).size;
-      alturaPantalla = size.height;
-      anchoPantalla = size.width;
-    });
-    
-    
-    DropdownButtonFormField(items: <DropdownMenuItem>[], onChanged: (value) {});
-   
-
-    Widget retornarMensajes(DateTime dia) {
-
-      listaMensajes.forEach((element) => {
-        if(dia.day == element.fecha.day){
-        eventos ++
-
-        }
-      });
-
-      return Align(
-                        alignment: Alignment.bottomRight,
-                        child: Container(
-                            width: 20,
-                            height: 20,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle, color: Colors.cyan),
-                            child: Text(
-                              eventos.toString(),
-                              style: TextStyle(
-                                  color: Color.fromARGB(255, 48, 92, 174),
-                                  fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.center,
-                            )));
     }
+    
 
-    Widget retornarContenido() {
+    //Metodo que se llamara cuando seleccionemos cualquier dia
+  dia_seleccionadoAdmin(DateTime dia, DateTime diaFocus) {
+    setState(() {
+      hoy = dia;
+    });
+    diaSeleccionado = dia;
+   retornarLista(listaEventos, diaSeleccionado);
+  }
+  
+  Widget retornarContenido() {
       if(retornarCalendario) {
         return Container(
         padding: const EdgeInsets.all(20.0),
@@ -294,25 +312,62 @@ class TutoriasAdminState extends State<TutoriasAdmin> {
                 llame a esta funcion, la cual cambia el estado del dia seleccionado para que se le apliquen
                 los estilos (los estilos es basicamente que su background cambie)
                 */
-                onDaySelected: dia_seleccionadoAdmin,
+                
                 //Esto es lo mas importante y dificil de esta parte ASI QUE ATENTOS
                 /*Con calendarBuilders puedo acceder a metodos por defectos de la libreria y
                 modificarlos
                 */
                 calendarBuilders: CalendarBuilders(
+                
                    markerBuilder: (context, day, events) {
                     //Cantidad de "citas"
                     // AQUI va a haber que cambiarlo en funcion a lo que recuperemos de la API //
                     //Recorro todos los dias el mes en el que estoy
+                    eventos = 0;
+                    for(int i = 0; i < listaEventos.length; i ++) {
+                      
+                    if(DateTime.utc((day.day).toInt(), diaSeleccionado.month, diaSeleccionado.year) == listaEventos[i].fecha){
+                     eventos = eventos + 1;
+                     }
+                     
+                    }
+                    
+                    return Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.cyan),
+                            child: Text(
+                              eventos.toString(),
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 48, 92, 174),
+                                  fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            )));
+                        
+              }
+
                   
-                   retornarMensajes(day);
-                  },
+                  /*Modifico el metodo markerBuilder, este metodo es el que me permite agregarle
+                  a cada celda del calendario contenido, en este caso le agrego un numero ya definido
+                  este numero equivaldra en la version final al numero de examenes que tenga el alumno 
+                  ese dia, si no tiene obviamente no aparecedera nada
+                  */
+                
                 ),
+                onDaySelected: dia_seleccionadoAdmin,
+                onPageChanged: (focusedDay) => {
+        
+                },
+               
+                
               ),
             ),
             FloatingActionButton(
         onPressed: () {
-     print(hoy.month);
+          print(hoy.month);
           setState(() {
             retornarCalendario = false;
             retornarTutoria = true;
