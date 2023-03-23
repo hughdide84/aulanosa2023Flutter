c18fa99bcff758d8f8ae083ce6bf6e5527335b5b
rober0002 <roberto_b31@hotmail.com>
2023-03-23 10:25:51 +0100

    admin_alumnos_v1

--- Proyecto_Principal/aulanosa_app/lib/admin/admin_alumnos.dart
+++ Proyecto_Principal/aulanosa_app/lib/admin/admin_alumnos.dart
@@ -15,10 +15,40 @@ class AdminAlumnos extends StatefulWidget {
 class _AdminAlumnosState extends State<AdminAlumnos> {
   @override
    Widget build(BuildContext context) => Scaffold(
+    
     appBar: AppBar(
       backgroundColor: Color.fromARGB(255, 48, 92, 174),
-      title: Text('Editar Alumnos'),
+      title: Text('Alumnos'),
       leading: MenuWidget(),
     ),
   );
+
+
+    //Metodo para determinar que apartado tiene que mostrar dentro de cada alumno externo
+  String determinarApartado(int index) {
+    if (index == 0) {
+      return "nombre";
+    } else if (index == 1) {
+      return "tipo";
+    } else if (index == 2) {
+      return "telefono";
+    } else if (index == 3) {
+      return "email";
+    } else if (index == 4) {
+      return "universidad";
+    } else if (index == 5) {
+      return "titulacion";
+    } else if (index == 6) {
+      return "especialidad";
+    } else if (index == 7) {
+      return "evaluacion";
+    } else if (index == 8) {
+      return "horario";
+    } else if (index == 9) {
+      return "inicio";
+    } else if (index == 10) {
+      return "fin";
+    }
+    return '';
+  }
 }
